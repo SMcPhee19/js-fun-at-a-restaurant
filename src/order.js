414b9a848cf4c98b6f93156d6f74be3846eb1c27
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-12-11 16:56:46 -0700

    feat: complete order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
+function takeOrder(order, deliveryOrdersArray) {
+  if (deliveryOrdersArray.length < 3) {
+    deliveryOrdersArray.push(order)
+  }
+  return deliveryOrdersArray  
+}
+
+function refundOrder(orderNumber, orderArray) {
+  orderIndex = orderArray.findIndex(order => order.orderNumber == orderNumber)
+  if (orderIndex != -1) {
+    orderArray.splice(orderIndex, 1)
+  }
+  return orderArray
+}
+
+function listItems(ordersArray) {
+  itemNames = []
+  ordersArray.forEach(order => {
+    itemNames.push(order.item)
+  });
+
+  return itemNames.join(', ')
+}
+
+function searchOrder(orderArray, itemName) {
+  orderIndex = orderArray.findIndex(order => order.item == itemName)
+
+  if (orderIndex != -1) {
+    return true
+  } else {
+    return false    
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
